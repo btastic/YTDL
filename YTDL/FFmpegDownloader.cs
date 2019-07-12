cf54b1402908781d77024d5541a1fcec98071826
Ben Konsemüller <bkonsem@gmail.com>
2019-07-12 21:05:20 +0200

    added own ffmpeg downloader class

---
+++ YTDL/FFmpegDownloader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace YTDL
+{
+    public static class FFmpegDownloader
+    {
+        public static async Task<bool> DownloadFFmpegAsync()
+        {
+            var architecture = Environment.Is64BitProcess ? "win64" : "win32";
+            
+            var ffmpegVersion = "4.1.3";
+            var ffmpegFolderName = $"ffmpeg-{ffmpegVersion}-{architecture}-static";
+            var ffmpegZipName = $"{ffmpegFolderName}.zip";
+            var ffmpegDownloadUrl = $"https://ffmpeg.zeranoe.com/builds/{architecture}/static/{ffmpegZipName}";
+
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    await webClient.DownloadFileTaskAsync(new Uri(ffmpegDownloadUrl), ffmpegZipName);
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine(ex);
+                    return false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return false;
+                }
+            }
+
+            ZipFile.ExtractToDirectory($".\\{ffmpegZipName}", $".\\{ffmpegFolderName}\\", true);
+
+            File.Copy($".\\{ffmpegFolderName}\\{ffmpegFolderName}\\bin\\ffmpeg.exe", ".\\ffmpeg.exe");
+            File.Copy($".\\{ffmpegFolderName}\\{ffmpegFolderName}\\bin\\ffprobe.exe", ".\\ffprobe.exe");
+
+            Directory.Delete($".\\{ffmpegFolderName}\\", true);
+
+            return true;
+        }
+    }
+}
